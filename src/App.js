114215c002b91c4b125f386ff7e38e515b7799fe
Joydeep Nandi <80601095+joydeepnandi@users.noreply.github.com>
2021-07-20 01:29:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Join from "./component/Join/Join";
+import Chat from "./component/Chat/Chat";
+
+function App() {
+
+  return (
+    <div className="App"> 
+      <Router>
+      <Route exact path ="/" component={Join} />
+      <Route path ="/chat"  component={Chat}/>
+
+      </Router>
+        
+    </div>
+  );
+}
+
+export default App;
